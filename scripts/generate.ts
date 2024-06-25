dec4fbd15a65b256ab1a32ce7930e1dd4629cfd7
Miguel5252 <msantaolalla@gmail.com>
2024-06-25 20:39:01 +0200

    pruebas

--- scripts/generate.ts
+++ scripts/generate.ts
@@ -62,8 +62,8 @@ async function generateEmbeddings() {
   //Juntar DOCS y hacerles el Split
   const docs = [...web_docs, ...pdf_docs]
   const web_splitter = RecursiveCharacterTextSplitter.fromLanguage("html", {
-    chunkSize: 400,
-    chunkOverlap: 30,
+    chunkSize: 500,
+    chunkOverlap: 20,
   });
   const splittedDocs = await web_splitter.splitDocuments(docs);
 

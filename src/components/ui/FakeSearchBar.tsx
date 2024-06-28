771e5c07745417f95563c37f4e3c1bcb453024d8
Miguel5252 <msantaolalla@gmail.com>
2024-06-28 09:14:53 +0200

    fakeSearBar -> input element to div

--- src/components/ui/FakeSearchBar.tsx
+++ src/components/ui/FakeSearchBar.tsx
@@ -5,11 +5,8 @@ import { botContext } from "@/contexts/BotContextProvider"
 function FakeSearchBar(){
   const {handleShowBot} = useContext(botContext)
   return(
-    <input
-      type="text"
-      className="max-w-2xl h-10 block w-full rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-400 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-100 sm:text-sm sm:leading-6"
-      placeholder="Ask the assistant anything about me"
+    <div className="text-left cursor-pointer max-w-[500px] h-10 w-full rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-400 ring-1 ring-inset ring-gray-400 placeholder:text-gray-400 flex items-center focus:ring-2 focus:ring-inset focus:ring-indigo-100 sm:text-sm sm:leading-6"
       onClick={()=>handleShowBot(true)}
-  />
+  >Ask me anything...</div>
   )
 } export default FakeSearchBar

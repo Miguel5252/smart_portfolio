808f3c4eef79770cf6c6223051fb024dc5312f80
Miguel5252 <msantaolalla@gmail.com>
2024-08-04 14:17:15 +0200

    cron job

--- src/app/api/cron/read_db/route.ts
+++ src/app/api/cron/read_db/route.ts
@@ -3,7 +3,18 @@ import { getVectorStore } from "@/lib/astradb";
 export const revalidate = 0;
 
 export async function GET() {
-  const db = await getVectorStore();
-  console.log('prueba', db)
-  return Response.json({ data: true});
+  const vectorStore = await getVectorStore();
+  
+  // Realizar una búsqueda simple para obtener todos los documentos
+  const results = await vectorStore.similaritySearch("", 10);  // El número 1000 es el límite de documentos a recuperar
+
+  console.log('Documentos en la colección:', results);
+  
+  // Extraer información relevante de los resultados
+  const documents = results.map(result => ({
+    pageContent: result.pageContent,
+    metadata: result.metadata
+  }));
+
+  return Response.json({ data: documents });
 }
